a77cd4d437b63e2567832d7331fd269168ea29ff
williamsilveiraf <williamsilveiraf@hotmail.com>
2022-05-16 11:49:29 -0300

    feat(cards): set getters

--- poo2/cards/main.py
+++ poo2/cards/main.py
@@ -7,19 +7,34 @@ class Card:
         self.cardType = cardType
 
 class Cards:
-    def __init__(self):
+    def __init__(self, amtPlayers):
+        self.all = self.getCards()
+        self.players = self.getPlayers(amtPlayers)
+
+    def getCards(self):
         suits = ['hearts', 'clubs', 'diamonds', 'spades']
         allCards = cardList.getAll()
-        self.all = []
+        cards = []
+
         for suit in suits:
             for card in allCards:
-                self.all.append(vars(Card(suit, card[0])))
+                cards.append(vars(Card(suit, card[0])))
+        return cards
 
     def sortCards(self):
-        print()
+        random.shuffle(self.all)
 
-cards = Cards()
+    def getPlayers(self, amtPlayers):
+        self.sortCards()
+        cardsAmt = len(self.all) // amtPlayers
+        players = {}
+        for player in range(0, amtPlayers):
+            players[player] = []
+            for _ in range(0, cardsAmt):
+                card = self.all.pop()
+                players[player].append(card)
+        return players
 
-cards.sortCards()
+cards = Cards(amtPlayers=9)
 
-print(len(cards.all))
+print(cards.players)
